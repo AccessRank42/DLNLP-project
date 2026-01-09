28f7058a7c823b0143bcb44bd415df44a4ac28b9
Gabriel Haubenburger <e11840531@student.tuwien.ac.at>
2026-01-09 22:41:10 +0100

    some work on the decoder

--- model.py
+++ model.py
@@ -21,6 +21,8 @@ class DocAndQuesEncoder(ch.Chain):
             self.encLSTM = L.NStepLSTM(1, emb_mat.shape[1], hid_size, dropout)
             # self.projectionLayer = ch.Sequential(L.Linear(hid_size, hid_size), F.tanh)
             self.projectionLayer = L.Linear(hid_size, hid_size)
+
+            # Sentinel vectors for D, Q
             self.sentielD = ch.Parameter(np.random.randn(emb_mat.shape[1], 1)) #TODO: shape?
             self.sentielQ = ch.Parameter(np.random.randn(emb_mat.shape[1], 1)) #TODO: shape?
 
@@ -54,7 +56,8 @@ class CoattentionEncoder(ch.Chain):
     def __init__(self, dropout, hid_size):
         super(CoattentionEncoder, self).__init__()
         with self.init_scope():
-            self.biLSTM = L.NStepBiLSTM(1, 3*hid_size, 2*hid_size, dropout)
+            # self.biLSTM = L.NStepBiLSTM(1, 3*hid_size, 2*hid_size, dropout)
+            self.biLSTM = L.NStepBiLSTM(1, 3*hid_size, hid_size, dropout)
 
         self.dropout = dropout
 
@@ -83,9 +86,11 @@ class CoattentionEncoder(ch.Chain):
 
         # U = [u_1, ..., u_m] for u_t = bi-directional LSTM(u_{t-1}, u_{t+1}, [d_t, c^D_t]) \in \R^{2l} 
         DCd = F.concat([D, Cd], axis=2)
+        # print(DCd.shape)
         DCd = [DCd[i] for i in range(DCd.shape[0])]
         _, _, U = self.biLSTM(hx, cx, DCd)
         U = F.stack(U, axis=0)
+        # print(U.shape)
 
 
         #TODO: remove sentinel vect - before or after calc U?
@@ -95,18 +100,30 @@ class HighwayMaxout(ch.Chain):
     def __init__(self, dropout, hid_size, maxout_pool_size):
         super(HighwayMaxout, self).__init__()
         with self.init_scope():
-            self.projectionLayer_alpha = ch.Sequential(L.Linear(5*hid_size, hid_size, nobias=True), F.tanh)
-            self.max1 = L.Maxout(3*hid_size, hid_size, maxout_pool_size)
+            # self.projectionLayer = ch.Sequential(L.Linear(5*hid_size, hid_size, nobias=True), F.tanh)
+            self.projectionLayer = L.Linear(hid_size)
+            # self.max1 = L.Maxout((3*hid_size)**2, hid_size, maxout_pool_size)
+            self.max1 = L.Maxout((3*hid_size)**2, hid_size, maxout_pool_size)
             self.max2 = L.Maxout(hid_size, hid_size, maxout_pool_size)
             self.max3 = L.Maxout(2*hid_size, 1, maxout_pool_size)
 
         self.dropout = dropout
 
-    def forward(self, U):
-        r = None #TODO
-        m_t_1 = self.max1(F.concat([U, r], axis=0))
+    def forward(self, U, h_i, u_s_i, u_e_i):
+        # print(h_i.shape)
+        # print(u_s_i.shape)
+        # print(u_e_i.shape)
+        # r_in = F.concat([h_i, u_s_i, u_e_i], axis=2)
+        r_in = F.concat([h_i, u_s_i, u_e_i], axis=1)
+        print(r_in.shape)
+        r = F.tanh(self.projectionLayer(r_in))
+        r = F.swapaxes(F.tile(r, (U.shape[1],1,1)), 0, 1)
+        print(r.shape)
+        print(U.shape)
+        Ur = F.concat([U, r], axis=2)
+        m_t_1 = self.max1(Ur)
         m_t_2 = self.max2(m_t_1)
-        hmn = self.max3(F.concat([m_t_1, m_t_2], axis=0))
+        hmn = self.max3(F.concat([m_t_1, m_t_2], axis=1))
         return hmn
 
 
@@ -114,7 +131,9 @@ class DynamicPointingDecoder(ch.Chain):
     def __init__(self, dropout, hid_size, maxout_pool_size, dyn_dec_max_it):
         super(DynamicPointingDecoder, self).__init__()
         with self.init_scope():
-            self.encLSTM = L.NStepLSTM(1, hid_size, hid_size, dropout)
+            # self.decLSTM = L.NStepLSTM(1, hid_size, hid_size, dropout)
+            # self.decLSTM = L.LSTM(hid_size, hid_size)
+            self.decLSTM = L.LSTM(None, hid_size)
 
             self.hmn_start = HighwayMaxout(dropout, hid_size, maxout_pool_size)
             self.hmn_end = HighwayMaxout(dropout, hid_size, maxout_pool_size)
@@ -122,13 +141,34 @@ class DynamicPointingDecoder(ch.Chain):
         self.dropout = dropout
         self.dyn_dec_max_it = dyn_dec_max_it
 
-    def forward(self, U):
+    def forward(self, U, hx=None, cx=None):
+        # h_i = U
+        s_i = 0
+        e_i = U.shape[1]-1
+
+        # u_s_i = U[:,s_i:s_i+1,:]
+        u_s_i = U[:,s_i,:]
+        # u_e_i = U[:,e_i:e_i+1,:]
+        u_e_i = U[:,e_i,:]
+        
         #TODO
         for i in range(self.dyn_dec_max_it):
-            alpha = self.hmn_start(U)
+            
+            # h_i = [U[i] for i in range(h_i.shape[0])]
+            
+            # hx, cx, h_i = self.decLSTM(hx, cx, h_i) #TODO: reuse hx, cx?
+            # h_i = F.stack(h_i, axis=0)
+            # u_se_i = F.concat([u_s_i, u_e_i], axis=2) #TODO: should axis=1 instead?
+            u_se_i = F.concat([u_s_i, u_e_i], axis=1) #TODO: should axis=1 instead?
+            h_i = self.decLSTM(u_se_i) #TODO: unsure if and how h_i should also be fed in
+
+            alpha = self.hmn_start(U, h_i, u_s_i, u_e_i)
             # s = F.max(alpha)
-            beta = self.hmn_end(U)
+            beta = self.hmn_end(U, h_i, u_s_i, u_e_i)
             # e = F.max(beta)
+
+            u_s_i = U[:,s_i:s_i+1,:]
+            u_e_i = U[:,e_i:e_i+1,:]
         
 
         # return s[-1], e[-1]
